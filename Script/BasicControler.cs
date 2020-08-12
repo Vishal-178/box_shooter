1e784a2115083782ca09b2cb4ab6461e3c47bfbf
vishal singh <39831629+Vishal-178@users.noreply.github.com>
2020-08-12 14:29:00 +0530

    Add files via upload

---
+++ Script/BasicControler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicControler : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("horizontal Input = " +Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Script/BasicTargetMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public bool DoSpin = true;
+    public float SpinSpeed = 180f;
+    public bool DoMotion = false;
+    public float MotionMagnitude = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (DoSpin)
+            gameObject.transform.Rotate(Vector3.up * SpinSpeed * Time.deltaTime);
+        if(DoMotion)
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * MotionMagnitude);
+    }
+}
